8a13d30da0132c53e8e3ed81c08c475437b9e8a1
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-07-14 18:04:03 -0700

    More safari sizing issues

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main class="flex">
-	<div class="flex-col justify-center mx-auto my-6 mb-8 ">
+	<div class="flex-col justify-center mx-auto my-6 mb-12 ">
 		<p class="flex justify-center font-normal text-sm text-gray-600">
 			Interested in producing?
 		</p>
